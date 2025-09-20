d0b156a0cb1db2e03eac3e17edd226844bec1937
Joseph Forsyth <jjforsyth15@gmail.com>
2025-09-19 18:06:24 -0700

    Class 9/19

--- Comp_584_Server/Program.cs
+++ Comp_584_Server/Program.cs
@@ -30,6 +30,8 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseAuthorization();
 
 app.MapControllers();
