c4b6d0b567318240f25bb59186616120d9c05b49
Joseph Forsyth <jjforsyth15@gmail.com>
2025-09-26 18:14:52 -0700

    Class 9/26

---
+++ WorldModel/City.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorldModel;
+
+[Table("City")]
+public partial class City
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [Column("countryID")]
+    public int CountryId { get; set; }
+
+    [Column("name")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    [Column("latitude")]
+    [StringLength(10)]
+    [Unicode(false)]
+    public string Latitude { get; set; } = null!;
+
+    [Column("longitude")]
+    [StringLength(10)]
+    [Unicode(false)]
+    public string Longitude { get; set; } = null!;
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
