03b41ca3e594a8984faa868172acf43dbc3a389d
Joseph Forsyth <jjforsyth15@gmail.com>
2025-10-03 17:56:56 -0700

    Class 10/03/2025

---
+++ Comp_584_Server/Controllers/SeedController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorldModel;
+
+namespace Comp_584_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController(Comp584Context context) : ControllerBase
+    {
+        [HttpPost ("Countries")]
+        public async Task<ActionResult> PostCountries()
+        {
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPost ("Cities")]
+        public async Task<ActionResult> PostCities()
+        {
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
--- Comp_584_Server/Program.cs
+++ Comp_584_Server/Program.cs
@@ -1,10 +1,18 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using WorldModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<Comp584Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen(c =>
